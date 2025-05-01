c333e1ba188d38a9a9123f060596baae11949324
Dajs4 <176528084+Dajs4@users.noreply.github.com>
2025-04-30 19:02:17 -0500

    style(config): Ajustar el formateo en tailwind.config.js según reglas estrictas de Prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './index.html',
-    './src/**/*.{js,jsx}'
-  ],
+  content: ['./index.html', './src/**/*.{js,jsx}'],
   theme: {
     extend: {
       colors: {
-        'sena-green': '#39A900',  // Color principal del SENA
-        'sena-dark': '#00324D',   // Color secundario del SENA
+        'sena-green': '#39A900', // Color principal del SENA
+        'sena-dark': '#00324D', // Color secundario del SENA
       },
     },
   },
